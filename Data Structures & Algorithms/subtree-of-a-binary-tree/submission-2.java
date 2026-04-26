1bc0b9741c5cdc08a5de9d2e886aa232513b1161
BORIGARLA MADHULATHA <madhulathaborigarla58367@gmail.com>
2026-04-26 12:07:22 +0530

    Add: subtree-of-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-2.java
@@ -0,0 +1,19 @@
+
+
+class Solution {  
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) return false;
+        if (isSameTree(root, subRoot)) return true;
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+
+        return isSameTree(p.left, q.left) &&
+        p.val == q.val &&
+        isSameTree(p.right, q.right);
+    }
+}
