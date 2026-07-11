83850fedee167c1a9910c88d748eae38aafe024b
BORIGARLA MADHULATHA <madhulathaborigarla58367@gmail.com>
2026-07-12 00:15:24 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+      
+        int n = prices.length;
+      
+        int maxProfit=Integer.MIN_VALUE;
+        int minPrice = prices[0];
+       for(int i=0;i<n;i++){
+        if(prices[i]<minPrice) minPrice =prices[i];
+            
+            int profit = prices[i]-minPrice;
+            
+            maxProfit = Math.max(profit, maxProfit);
+        }
+        return maxProfit;
+        
+    }
+}
